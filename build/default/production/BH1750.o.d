469a0d729ae9764ba9e651e5bb1ec25f9c4fd90a
EMIC Compiler <emic-compiler@emic.io>
2026-02-23 14:33:13 +0000

    Code generation: 2026-02-23 14:33:13 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: hdr-e-crys-b-lux-1
    Project: pruebasmodulos
    User: lugaro95

--- build/default/production/BH1750.o.d
+++ build/default/production/BH1750.o.d
@@ -1,20 +1,20 @@
-/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/build/default/production/BH1750.o: \
- /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/BH1750.c \
- /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/BH1750.h \
- /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/gpio.h \
- /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/pins.h \
- /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/I2C1.h \
- /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/systemTimer.h \
- /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/system.h
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/build/default/production/BH1750.o: \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/BH1750.c \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/BH1750.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/gpio.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/pins.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/I2C1.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/systemTimer.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/system.h
 
-/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/BH1750.h:
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/BH1750.h:
 
-/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/gpio.h:
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/gpio.h:
 
-/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/pins.h:
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/pins.h:
 
-/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/I2C1.h:
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/I2C1.h:
 
-/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/systemTimer.h:
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/systemTimer.h:
 
-/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/system.h:
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/system.h:
--- build/default/production/stream.o.d
+++ build/default/production/stream.o.d
@@ -1,5 +1,5 @@
-/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/build/default/production/stream.o: \
- /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/stream.c \
- /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/stream.h
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/build/default/production/stream.o: \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/stream.c \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/stream.h
 
-/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/stream.h:
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com/PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/stream.h:
