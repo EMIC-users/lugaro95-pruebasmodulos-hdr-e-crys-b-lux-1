ef5e7da97a0db2c374afac0998730a4b64277d60
EMIC Compiler <emic-compiler@emic.io>
2026-02-23 13:38:41 +0000

    Code generation: 2026-02-23 13:38:41 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: hdr-e-crys-b-lux-1
    Project: pruebasmodulos
    User: lugaro95

---
+++ build/default/production/main.o.d
@@ -0,0 +1,50 @@
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/build/default/production/main.o: \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/main.c \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/systemConfig.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/systemTimer.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/led_Led1.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/led_Led2.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/led_Led3.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/timer_api1.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/Transport.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/stream.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/EmicFieldBus.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/streamIn.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/streamOut.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/BH1750.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/LUX.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/system.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/userFncFile.h \
+ /root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/system.c
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/systemConfig.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/systemTimer.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/led_Led1.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/led_Led2.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/led_Led3.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/timer_api1.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/Transport.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/stream.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/EmicFieldBus.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/streamIn.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/streamOut.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/BH1750.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/LUX.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/system.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/inc/userFncFile.h:
+
+/root/Dropbox/EMIC/TARGETS/luciano.rottoli__rfindustrial.com//PruebasModulos/HDR_E-CRYS-B_LUX_1/system.c:
